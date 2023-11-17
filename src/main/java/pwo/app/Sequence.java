65ca38c7961942a3686ee221a90375468ed470fc
student <student@localhost>
2023-11-17 16:27:49 +0100

    Dystrybucja 1

--- src/main/java/pwo/app/Sequence.java
+++ src/main/java/pwo/app/Sequence.java
@@ -7,6 +7,17 @@ package pwo.app;
 public class Sequence {
 
     public static void main(String[] args) {
-        (new SeqToFileApp()).run(args);
+
+        switch (args.length) {
+            case 3:
+                (new SeqToOutApp()).run(args);
+                break;
+            case 4:
+                (new SeqToFileApp()).run(args);
+                break;
+            default:
+                System.out.println("!Illegal arguments\n"
+                        + "Legal usage: seqName from to [fileName]");
+        }
     }
 }
